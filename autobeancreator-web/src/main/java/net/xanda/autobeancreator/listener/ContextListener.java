4567bf4e8165968e3f462d23a36bc7e4739e7ab3
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2016-02-14 17:59:42 +0000

    remove unused line

--- autobeancreator-web/src/main/java/net/xanda/autobeancreator/listener/ContextListener.java
+++ autobeancreator-web/src/main/java/net/xanda/autobeancreator/listener/ContextListener.java
@@ -12,10 +12,8 @@ import java.io.File;
 public class ContextListener implements ServletContextListener {
     /**
      *      * Initialize log4j when the application is being started
-     *      
      */
     @Override
-
     public void contextInitialized(ServletContextEvent event) {
         // initialize log4j here
         ServletContext context = event.getServletContext();
@@ -25,10 +23,7 @@ public class ContextListener implements ServletContextListener {
         PropertyConfigurator.configure(fullPath);
     }
 
-
     @Override
-
-
     public void contextDestroyed(ServletContextEvent event) {
         // do nothing
     }
