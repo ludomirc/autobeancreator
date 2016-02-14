2f615185523f90f655b58b6ccc33d089e7641401
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2016-02-14 22:18:27 +0000

    add ddl sql

---
+++ sql/ddl.sql
@@ -0,0 +1,47 @@
+CREATE TABLE attributetype
+(
+    att_type_id INT(11) PRIMARY KEY NOT NULL,
+    att_name VARCHAR(255) NOT NULL
+);
+CREATE TABLE bean
+(
+    bean_id INT(11) PRIMARY KEY NOT NULL,
+    bean_name VARCHAR(255) NOT NULL,
+    proj_id INT(11) DEFAULT '0' NOT NULL,
+    friendly_name VARCHAR(255) NOT NULL
+);
+CREATE TABLE beanattribute
+(
+    bean_att_id INT(11) PRIMARY KEY NOT NULL,
+    bean_att_name VARCHAR(255) NOT NULL,
+    bean_id INT(11) DEFAULT '0' NOT NULL,
+    att_type_id INT(11) DEFAULT '0' NOT NULL,
+    form_hidden TINYINT(4) DEFAULT '0' NOT NULL,
+    primary_key TINYINT(4) DEFAULT '0' NOT NULL,
+    compolsory_field TINYINT(4) DEFAULT '0' NOT NULL,
+    friendly_name VARCHAR(255) NOT NULL,
+    `order` INT(11) DEFAULT '0' NOT NULL
+);
+CREATE TABLE beanload
+(
+    bl_id INT(11) PRIMARY KEY NOT NULL,
+    bl_name VARCHAR(255) NOT NULL,
+    bean_id INT(11) DEFAULT '0' NOT NULL,
+    blt_id INT(11) DEFAULT '0' NOT NULL
+);
+CREATE TABLE beanloadattribute
+(
+    blatt_id INT(11) PRIMARY KEY NOT NULL,
+    bean_att_id INT(11) DEFAULT '0' NOT NULL,
+    bl_id INT(11) DEFAULT '0' NOT NULL
+);
+CREATE TABLE beanloadtype
+(
+    blt_id INT(11) PRIMARY KEY NOT NULL,
+    blt_name VARCHAR(255) NOT NULL
+);
+CREATE TABLE project
+(
+    proj_id INT(11) PRIMARY KEY NOT NULL,
+    proj_name VARCHAR(255) NOT NULL
+);
