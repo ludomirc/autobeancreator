3f17aab753389d81f119ab78874fcd113cf50202
beniamin.czaplicki@gmail.com <beniamin.czaplicki@gmail.com>
2016-02-15 19:02:03 +0000

    update db ddl script add autoincrement to primary key kolumn

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,18 +1,18 @@
 CREATE TABLE attributetype
 (
-    att_type_id INT(11) PRIMARY KEY NOT NULL,
+    att_type_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     att_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE bean
 (
-    bean_id INT(11) PRIMARY KEY NOT NULL,
+    bean_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     bean_name VARCHAR(255) NOT NULL,
     proj_id INT(11) DEFAULT '0' NOT NULL,
     friendly_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE beanattribute
 (
-    bean_att_id INT(11) PRIMARY KEY NOT NULL,
+    bean_att_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     bean_att_name VARCHAR(255) NOT NULL,
     bean_id INT(11) DEFAULT '0' NOT NULL,
     att_type_id INT(11) DEFAULT '0' NOT NULL,
@@ -24,24 +24,24 @@ CREATE TABLE beanattribute
 );
 CREATE TABLE beanload
 (
-    bl_id INT(11) PRIMARY KEY NOT NULL,
+    bl_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     bl_name VARCHAR(255) NOT NULL,
     bean_id INT(11) DEFAULT '0' NOT NULL,
     blt_id INT(11) DEFAULT '0' NOT NULL
 );
 CREATE TABLE beanloadattribute
 (
-    blatt_id INT(11) PRIMARY KEY NOT NULL,
+    blatt_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     bean_att_id INT(11) DEFAULT '0' NOT NULL,
     bl_id INT(11) DEFAULT '0' NOT NULL
 );
 CREATE TABLE beanloadtype
 (
-    blt_id INT(11) PRIMARY KEY NOT NULL,
+    blt_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     blt_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE project
 (
-    proj_id INT(11) PRIMARY KEY NOT NULL,
+    proj_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     proj_name VARCHAR(255) NOT NULL
 );
